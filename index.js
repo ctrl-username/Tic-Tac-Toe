bf46d904a95944a4aabc48f8dd1e0ad1332471a4
ctrl-username <243785975+ctrl-username@users.noreply.github.com>
2026-05-29 21:00:24 +0100

    create game board IIFE with closures

---
+++ index.js
@@ -0,0 +1,14 @@
+// and let the js coding begin, it's been a while
+// this sure as hell will be messy
+//track board state
+
+const gameBoard = (() => {
+  let board = ["", "", "", "", "", "", "", "", ""];
+  const getBoard = () => board;
+  const updateCell = (index, marker) => {
+    if (board[index != ""]) return false;
+    board[index] = marker;
+    return true;
+  };
+  const reset = () => board.fill("");
+})();
