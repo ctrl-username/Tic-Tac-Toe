5e0a09aae6dd7b619ab938b3fe65cd7e1be5b3cb
ctrl-username <243785975+ctrl-username@users.noreply.github.com>
2026-05-29 21:04:46 +0100

    link js to html

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 // and let the js coding begin, it's been a while
 // this sure as hell will be messy
 //track board state
+const playGame = () => {
+  const gameBoard = (() => {
+    let board = ["", "", "", "", "", "", "", "", ""];
+    const getBoard = () => board;
+    const updateCell = (index, marker) => {
+      if (board[index != ""]) return false;
+      board[index] = marker;
+      return true;
+    };
+    const reset = () => board.fill("");
+  })();
+  console.log("game start");
+};
 
-const gameBoard = (() => {
-  let board = ["", "", "", "", "", "", "", "", ""];
-  const getBoard = () => board;
-  const updateCell = (index, marker) => {
-    if (board[index != ""]) return false;
-    board[index] = marker;
-    return true;
-  };
-  const reset = () => board.fill("");
-})();
+playGame();
