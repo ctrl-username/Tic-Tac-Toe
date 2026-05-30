887982a8e8c85170b3624119ba73c56e29669b21
ctrl-username <243785975+ctrl-username@users.noreply.github.com>
2026-05-30 21:51:29 +0100

    man not coding for months makes me feel dumb

--- index.js
+++ index.js
@@ -6,13 +6,15 @@ const playGame = () => {
     let board = ["", "", "", "", "", "", "", "", ""];
     const getBoard = () => board;
     const updateCell = (index, marker) => {
-      if (board[index != ""]) return false;
+      if (board[index] !== "") return false;
       board[index] = marker;
       return true;
     };
     const reset = () => board.fill("");
     return { getBoard, updateCell, reset };
   })();
+
+  const displayController = () => {};
   console.log("game start", gameBoard.getBoard());
 };
 
