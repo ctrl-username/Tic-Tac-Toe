907f7df5cf116f468d4ec9bd1eecab1784681ea6
ctrl-username <243785975+ctrl-username@users.noreply.github.com>
2026-05-31 10:29:23 +0100

    just add a new change

--- index.js
+++ index.js
@@ -36,6 +36,8 @@ const playGame = () => {
 
   const winConditions = () => {
     [0, 1, 2][(3, 4, 5)][(6, 7, 8)];
+    bameboard= gameBoard.board
+    if (gameBoard.board[0]
   };
   gameBoard.updateCell(1, "X");
   console.log("game start", gameBoard.getBoard());
